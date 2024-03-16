b16dfcc991e7e40cfa5e993d05cdb3cf49d4dc39
Naimul9 <naimulakib100@gmail.com>
2024-03-16 12:51:47 +0600

    card section

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,45 @@
 
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import Singlerecipe from './Singlerecipe'
+import SingleRecipe from './Singlerecipe'
 
 function App() {
   
 
+const [recipes, setRecipe] =useState([])
+
+useEffect(()=>{
+fetch("./recipies.json")
+.then((res)=>res.json())
+.then((data)=> {setRecipe(data)})
+}, [])
+
+console.log(recipes)
+
+
+
   return (
     <>
    <nav className='w-full lg:max-w-[1320px] lg:mx-auto font-lexend'>
-   <div class="navbar bg-base-100">
-  <div class="navbar-start">
-    <div class="dropdown">
-      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+   <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabindex="0" role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabindex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
       <li><a>Home</a></li>
       <li><a>Recipes</a></li>
       <li><a>About</a></li>
       <li><a>Search</a></li>
       </ul>
     </div>
-    <a class="btn btn-ghost text-[10px] lg:text-[20px]">Recipe Calories</a>
+    <a className="btn btn-ghost text-[10px] lg:text-[20px]">Recipe Calories</a>
   </div>
-  <div class="navbar hidden lg:flex">
-    <ul class="menu menu-horizontal px-1">
+  <div className="navbar hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
       <li><a>Home</a></li>
       <li><a>Recipes</a></li>
       <li><a>About</a></li>
@@ -31,20 +47,20 @@ function App() {
     </ul>
   </div>
 
-  <div class="flex-none gap-2">
-    <div class="form-control">
-      <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+  <div className="flex-none gap-2">
+    <div className="form-control">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
     </div>
-    <div class="dropdown dropdown-end">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
-           <img alt="Tailwind CSS Navbar component" src="../src/assets/images/Frame 5.png" />
+    <div className="dropdown dropdown-end">
+      <div tabindex="0" role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+           <img alt="Tailwind CSS Navbar component" src="../src/images/Frame 5.png" />
         </div>
       </div>
 
 
 
-      <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+      <ul tabindex="0" className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
         <li><a>Profile</a></li>
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
@@ -56,13 +72,56 @@ function App() {
 
 
    <main>
-
+{/* banner  */}
 <section className='max-w-[1320px] mx-auto h-[600px] '>
 
-<div className=' bg-[url(assets/images/Rectangle1.png)]'>
+<div className='bg-[]'>
+
+
+<img src="../src/images/Rectangle1.png"/>
+  
 
 
 </div>
+
+
+</section>
+
+{/* Our Recipies */}
+
+<section className='mt[100px] max-w-[1320px] mx-auto'>
+
+<h1 className='text-[40px] font-semibold'>Our Recipies</h1>
+<p className='mt-5 text-center w-[1000px] mx-auto font-normal'>
+Delight in our diverse recipes, from comforting classNameics like Chicken Curry and Spaghetti Bolognese to healthier options like Vegetable Stir Fry. Indulge in Grilled Salmon or decadent Chocolate Cake.</p>
+
+<div className='mt-7 flex'>
+
+<div className='w-2/3'>
+
+ 
+ 
+  <div className='text-start pr-4 my-6 grid grid-cols-2 gap-5 -ml-10 '>
+    
+    {recipes.map( (rp)=>(<SingleRecipe recipe={rp} ></SingleRecipe>))}
+   
+    
+  </div>
+
+
+</div>
+
+
+<div className='w-1/3' >
+Single 
+</div>
+
+
+</div>
+
+
+
+
 
 
 </section>
