c0e535db7abe062a1ef8886b51b490fafa5e456b
Naimul9 <naimulakib100@gmail.com>
2024-03-17 16:32:46 +0600

    Final Submission

--- src/App.jsx
+++ src/App.jsx
@@ -56,10 +56,10 @@ console.log(recipes)
    <div className="navbar bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
-      <div tabindex="0" role="button" className="btn btn-ghost lg:hidden">
+      <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabindex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
       <li><a>Home</a></li>
       <li><a>Recipes</a></li>
       <li><a>About</a></li>
@@ -81,21 +81,24 @@ console.log(recipes)
   <div className="flex-none gap-2">
    <label id="hide" className="input input-bordered  mx-auto  flex  rounded-[50px] h-[50px] "  >
    <button id="apply" className="btn btn-ghost btn-circle">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
     </button>
       <input type="text" placeholder="Search" className="input w-24 md:w-auto border-none " />
      
     </label>
     <div className="dropdown dropdown-end">
-      <div tabindex="0" role="button" className="btn btn-ghost btn-circle avatar">
+      <div tabIndex="0" role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
-           <img alt="Tailwind CSS Navbar component" src="../src/images/Frame 5.png" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
         </div>
       </div>
 
 
 
-      <ul tabindex="0" className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+      <ul tabIndex="0" className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
         <li><a>Profile</a></li>
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
@@ -139,7 +142,7 @@ Delight in our diverse recipes, from comforting classNameics like Chicken Curry 
  
   <div className='text-start pr-5 my-6 grid lg:grid-cols-2 grid-cols-1 gap-5 -ml-5 '>
     
-    {recipes.map((rp)=>(<SingleRecipe handleCount={handleCount} recipe={rp} ></SingleRecipe>))}
+    {recipes.map((rp)=>(<SingleRecipe  handleCount={handleCount} recipe={rp} ></SingleRecipe>))}
    
     
   </div>
